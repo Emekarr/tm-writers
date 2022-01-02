c974a87b82e0c82f2b4cf7be8f04c8348202efad
emekarr <aceanyanwu02@gmail.com>
2022-01-02 22:16:36 +0100

    built the admin model

---
+++ src/db/models/admin.ts
@@ -0,0 +1,85 @@
+import { Document, model, Schema } from 'mongoose';
+import { hash } from 'bcrypt';
+
+import CustomError from '../../utils/error';
+
+export interface Admin {
+	name: string;
+	email: string;
+	firsttname: string;
+	lastname: string;
+	password?: string;
+	phoneNumber: string;
+}
+
+export interface IAdminDocument extends Admin, Document {}
+
+const adminSchemaFields: Record<keyof Admin, any> = {
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+		maxlength: 10,
+		minlength: 1,
+	},
+	email: {
+		type: String,
+		required: true,
+		maxlength: 100,
+		trim: true,
+		unique: true,
+		validate(data: string) {
+			const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+			if (!regex.test(data))
+				throw new CustomError('invalid email provided', 400);
+		},
+	},
+	firsttname: {
+		type: String,
+		required: true,
+		trim: true,
+		maxlength: 10,
+		minlength: 1,
+	},
+	lastname: {
+		type: String,
+		required: true,
+		trim: true,
+		maxlength: 10,
+		minlength: 1,
+	},
+	password: {
+		type: String,
+		required: true,
+		validate(data: string) {
+			const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+			if (!regex.test(data))
+				throw new CustomError('invalid password provided', 400);
+		},
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+		trim: true,
+		maxlength: 10,
+		minlength: 1,
+	},
+};
+
+const AdminSchema = new Schema(adminSchemaFields, { timestamps: true });
+
+AdminSchema.pre('save', async function (this: IAdminDocument, next) {
+	if (this.isModified('password')) {
+		this.password = await hash(this.password!, 10);
+	}
+	next();
+});
+
+AdminSchema.method('toJSON', function (this: IAdminDocument) {
+	const admin = this.toObject();
+	delete admin.__v;
+	delete admin.password;
+	return admin;
+});
+
+export default model<IAdminDocument>('Admin', AdminSchema);
