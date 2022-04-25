56d86b53de875d66bc1911217fd065c5f2c78f7c
emekarr <aceanyanwu02@gmail.com>
2022-04-25 02:41:37 +0100

    fixed bug with assing orde rusecase

--- src/usecases/order/AssignOrderUseCase.ts
+++ src/usecases/order/AssignOrderUseCase.ts
@@ -1,5 +1,6 @@
 import { validateUpdateOrder } from '../../validators/orderValidators';
 import order_repository from '../../repository/mongodb/order_repository';
+import { Types } from 'mongoose';
 
 export default abstract class AssignOrderUseCase {
 	private static validateUpdateOrder = validateUpdateOrder;
@@ -7,7 +8,9 @@ export default abstract class AssignOrderUseCase {
 	private static repo = order_repository;
 
 	static async execute(data: { assignedTo: string }, orderId: string) {
-		const order = this.validateUpdateOrder(data);
+		const order = this.validateUpdateOrder({
+			assignedTo: new Types.ObjectId(data.assignedTo),
+		});
 		if (order.error) return `invalid json passed : ${order.error.message}`;
 		return await this.repo.updateById(orderId, order.value);
 	}
